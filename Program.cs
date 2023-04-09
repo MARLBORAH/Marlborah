cc9510dce0e49748fc7de7efd18a0a8380208158
MARLBORAH <130259894+MARLBORAH@users.noreply.github.com>
2023-04-08 19:06:36 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+/* Program.cs                                       Jason Thatcher
+ *                                                  December 2017
+ * 
+ * Entry to SlotMachine app.  Sets number of iterations below by
+ * changing value of "i" in for loop.
+ * 
+ * C#, Loop, Comments, Console.
+*/
+
+
+using System;
+
+namespace SlotMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SlotMachineApp myApp = new SlotMachineApp();
+            
+            for (int i = 0; i<100000; i++)
+            {
+                myApp.Play();
+            }
+            
+            myApp.DisplaySummary();
+                       
+            Console.ReadLine();
+            
+        }
+    }
+}
